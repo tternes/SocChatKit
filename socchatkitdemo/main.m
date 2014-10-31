3ad58d2ddd6a5ce90ee9a27fac579950523d3466
Thaddeus Ternes <thaddeus@bluetoo.co>
2014-10-30 22:51:16 -0500

    Initial Commit

---
+++ socchatkitdemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  socchatkitdemo
+//
+//  Created by Thaddeus Ternes on 10/30/14.
+//  Copyright (c) 2014 Bluetoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
